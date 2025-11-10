affbcdb9d3eb466cf204e058e8a88c368967d9ff
Tolfx <matteus@tolfx.dev>
2025-11-10 08:10:04 +0100

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, FileNotFoundError):
 
 setup(
     name='netbox-interface-view',
-    version='0.1.0',
+    version='0.1.1',
     description='NetBox Plugin for viewing interfaces in a grid layout with VLAN color-coding',
     long_description=long_description,
     long_description_content_type='text/markdown',
