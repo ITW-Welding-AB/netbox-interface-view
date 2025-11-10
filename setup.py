d65e7de1bf20176bd2538d2645dfb1833a322062
Tolfx <matteus@tolfx.dev>
2025-11-10 10:19:10 +0100

    Bump version to 0.1.6 in __init__.py and setup.py; update interface cell styles for improved theming

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except (IOError, FileNotFoundError):
 
 setup(
     name='netbox-interface-view',
-    version='0.1.5',
+    version='0.1.6',
     description='NetBox Plugin for viewing interfaces in a grid layout with VLAN color-coding',
     long_description=long_description,
     long_description_content_type='text/markdown',
